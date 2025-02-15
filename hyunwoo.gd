b3464781fc3f084d40e6115a3c65fb4be5851d53
StaRadian <rheehosung0627@gmail.com>
2025-02-15 20:52:38 +0900

    주석 수정

--- hyunwoo.gd
+++ hyunwoo.gd
@@ -4,7 +4,7 @@ var speed = 500  # 떨어지는 속도
 
 func _process(delta):
 	position.y += speed * delta
-	if position.y > Global.screen_size.y + 100:  # 화면 끝까지 내려가면 다시 위로
+	if position.y > Global.screen_size.y + 100:  # 화면 끝까지 내려가면 삭제
 		queue_free()  # 삭제
 		Global.score += 1
 		print(Global.score)
